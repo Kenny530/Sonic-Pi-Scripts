f4200746a0754e7021de37986343d5af7918f70e
Kenny530 <kenneth_garcia@davincischools.org>
2018-02-01 14:42:55 -0800

    Add files via upload

---
+++ stranger things.rb
@@ -0,0 +1,23 @@
+use_bpm 160
+play:e4
+sleep 2
+play:d4
+sleep 0.5
+play:d4
+sleep 0.5
+play:e4
+sleep 0.5
+play:d4
+sleep 0.5
+play:b3
+sleep 0.5
+play:g3
+sleep 0.5
+play:b3
+sleep 0.5
+play:d4
+sleep 0.5
+play:e4
+sleep 0.5
+play:d4
+sleep 0.5
