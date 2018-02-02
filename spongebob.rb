ebc4d632da3f29289e237f37f374f7a1cf58f390
Kenny530 <kenneth_garcia@davincischools.org>
2018-02-02 15:35:52 -0800

    Update spongebob.rb

--- spongebob.rb
+++ spongebob.rb
@@ -1,28 +1,30 @@
+# Name of Song: Spongebob Theme Song
+# Sheet Music and BPM use:https://www.sheetmusicdirect.com/es-ES/se/ID_No/102881/Product.aspx
 use_bpm 104
 live_loop :sponge do
   use_synth :tri
   play:e4
   sleep 2.5
   play:d4
-  sleep 1
+  sleep 0.5
   play:d4
-  sleep 1
+  sleep 0.5
   play:e4
-  sleep 1
+  sleep 0.5
   play:d4
-  sleep 1
+  sleep 0.5
   play:b3
-  sleep 1
+  sleep 0.5
   play:g3
-  sleep 1
+  sleep 0.5
   play:b3
-  sleep 1
+  sleep 0.5
   play:d4
-  sleep 1
+  sleep 0.5
   play:e4
-  sleep 1
+  sleep 0.5
   play:g4
-  sleep 1
+  sleep 0.5
 end
 live_loop:drums do
   sample :drum_heavy_kick
