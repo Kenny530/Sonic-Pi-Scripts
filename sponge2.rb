b7642896170c3539955f92581ebf199e77e6dd40
Kenny530 <kenneth_garcia@davincischools.org>
2018-02-12 13:54:33 -0800

    Update sponge2.rb

--- sponge2.rb
+++ sponge2.rb
@@ -1,7 +1,16 @@
 # Name of Song: Spongebob Theme Song
 # Sheet Music and BPM use:https://www.sheetmusicdirect.com/es-ES/se/ID_No/102881/Product.aspx
-# Changed BPM for a better song
 use_bpm 150
+x = 0.025
+y2 = :drum_cymbal_hard
+
+
+10.times do
+  sample y2,amp: x
+  sleep 1
+  x = x + 1
+end
+# The code will read top from bottom so that means it will read what the sample is and how much the amplitude will go up after every sleep
 live_loop :sponge do
   use_synth :tri
   play:e4
@@ -27,18 +36,7 @@ live_loop :sponge do
   play:g4
   sleep 0.5
 end
-sleep 6
-
-y2 = :drum_cymbal_hard
-10. times do
-  sample y2
-  sleep 1
-end
-
-sleep 2
-
-y = :drum_heavy_kick
 live_loop:drums do
-  sample y
-  sleep 1
+  sample :drum_heavy_kick
+  sleep 2
 end
