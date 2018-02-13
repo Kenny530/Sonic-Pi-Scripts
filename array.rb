189d2a7e8ff95d3f0ec95cfc6517876385bf7fc6
Kenny530 <kenneth_garcia@davincischools.org>
2018-02-13 13:56:52 -0800

    Update array.rb

--- array.rb
+++ array.rb
@@ -2,12 +2,14 @@ use_bpm 160
 
 my_leg = [:c2, :e2, :g2, :b2, :c3, :b2, :g2, :e2]
 index = 0
-
+#I put my index in order for my array to go from c2 all the way to e2.
 8.times do
   use_synth :saw
   with_fx :distortion do
     play my_leg [index]
+    #I chose index because it allows the array to start from c2 
     sleep 0.5
     index = index + 1
+    #The +1 allows the array to jump from note to note
   end
 end
